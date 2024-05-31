e0f628fde426ea91603e906ee2ca004cc5418c6e
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-05-31 17:25:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Niknock HD'
+description '24h Parking Prop'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+NKHD 24h Parking Prop
+
+Adds an new Prop named 'nkhd_parking_prop' with 2 example places.
+
+ 
